6d8b3215cecf6c1f39ff32f65b28040ae5e66057
jeremyisep <jebi61265@eleve.isep.fr>
2023-05-26 16:19:00 +0200

    cartes

---
+++ src/main/java/com/example/six_qui_prend/cartes.java
@@ -0,0 +1,17 @@
+package com.example.six_qui_prend;
+
+public class cartes {
+    private int number;
+    private int number_head;
+
+    public int getNumber_head() {
+        return number_head;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+
+
+}
