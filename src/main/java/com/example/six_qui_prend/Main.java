87a7d969070968704339222ac8f04a85898c97ec
Estelle2416 <116879722+Estelle2416@users.noreply.github.com>
2023-05-26 17:54:45 +0200

    start game

---
+++ src/main/java/com/example/six_qui_prend/Main.java
@@ -0,0 +1,11 @@
+package com.example.six_qui_prend;
+
+import javafx.application.Platform;
+public class Main {
+    public static void main (String[] args ){
+        Platform.startup(()->{
+            StartGame startGame = new StartGame();
+            startGame.show();
+        });
+    }
+}
