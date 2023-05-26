bce8931913600ad8aedd349e49b057b1ea31e5b0
Estelle2416 <116879722+Estelle2416@users.noreply.github.com>
2023-05-26 18:40:16 +0200

    test

--- src/main/java/com/example/six_qui_prend/Board.java
+++ src/main/java/com/example/six_qui_prend/Board.java
@@ -2,13 +2,16 @@ package com.example.six_qui_prend;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class Board extends Application{
     private GridPane grid;
-    public Began{
+    public firstwindow{
         grid = new GridPane();
         grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setVgap(10);
         grid.setHgap(10);
     }
+
+
 }
