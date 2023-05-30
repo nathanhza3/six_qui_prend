72fba8978ec3f07d5f31a6478c7d51242f53c310
Estelle2416 <116879722+Estelle2416@users.noreply.github.com>
2023-05-30 17:29:07 +0200

    test

--- src/main/java/com/example/six_qui_prend/Board.java
+++ src/main/java/com/example/six_qui_prend/Board.java
@@ -1,11 +1,17 @@
 package com.example.six_qui_prend;
+import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 
 public class Board extends Stage {
     private GridPane grid;
+
+
 
     public void firstwindow() {
         grid = new GridPane();
@@ -14,9 +20,12 @@ public class Board extends Stage {
         grid.setHgap(10);
         Scene scene = new Scene(grid, 300, 450);
         setScene(scene);
-        setTitle("Lancement du jeu");
+        setTitle("distribution");
 
-
+        // Ajouter des cartes à la grille
+        Image carte1 = new Image("/com/example/six_qui_prend/clientjavafx/ui/card/1.png");
+        ImageView imageView1 = new ImageView(carte1);
+        grid.add(imageView1, 1, 1);
     }
 
 
