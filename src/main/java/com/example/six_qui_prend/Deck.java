6c563fe52e1e9fc16f1ed484f5b092d9b48c91c2
jeremyisep <jebi61265@eleve.isep.fr>
2023-05-30 14:57:29 +0200

    deck

--- src/main/java/com/example/six_qui_prend/Deck.java
+++ src/main/java/com/example/six_qui_prend/Deck.java
@@ -1,18 +1,32 @@
 package com.example.six_qui_prend;
+import java.util.Arrays;
 
-
-import java.util.List;
-
+import java.util.Collections;
 public class Deck {
-    /*private List<Cartes> createDeck() {
-        ListCartes<> deck = new Cartes[104];
-        int index = 0;
+    private Cartes[] createDeck() {
+        Cartes[] deck = new Cartes[104];
+
         for (int i = 1; i <= 104; i++) {
 
-            deck[index] = new Card(i, points);
-            index++;
-        }
-        shuffleDeck(deck);
-        return deck;*/
-}
+            deck[i] = new Cartes(i, number_head(i));
 
+        }
+        Collections.shuffle(Arrays.asList(deck));
+        return deck;
+    }
+
+    private int number_head(int i){
+        if(i==55){
+            return 7;
+        } else if(i%10==0){
+            return 3;
+        } else if(i%5==0){
+            return 2;
+        } else if(i%11==0){
+            return 5;
+        } else {
+            return 1;
+        }
+
+    }
+}
